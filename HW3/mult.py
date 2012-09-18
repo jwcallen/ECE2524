02269aac4a763f4f938173956f18f0cf66735f31
Joe Callen <jwcallen@vt.edu>
2012-09-18 19:07:12 -0400

    Commit part 1 of HW3

---
+++ HW3/mult.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description="Process some numbers.")
+parser.parse_args()
+
+product = 1
+for line in iter(sys.stdin.readline, ""):
+	line = line.rstrip("\n")
+	if line == "":
+		print product
+		product = 1
+	try:
+		num = float(line)
+		product *= num
+	except ValueError as e:
+		sys.stderr.write(str(e))
+		sys.stderr.write("\n")
+		sys.stderr.flush()
+		sys.exit(1)
+
+print product
